7537510b4a0c7efeec045386d8c571c52f5b4ce4
Rong <joy40217@gmail.com>
2026-07-12 22:07:01 +0800

    feat: Refactor Astro site structure and enhance functionality
    - Updated package dependencies to include Preact and RSS support.
    - Redesigned the About, Blog, and Home pages to utilize a new BaseLayout component for consistent structure.
    - Introduced new components: BlogPost, Footer, Greeting, Header, Menu, Navigation, Social, and ThemeIcon for modular design.
    - Implemented a theme toggle feature for dark/light mode.
    - Added a global CSS file for styling and responsive design.
    - Created a MarkdownPostLayout for blog posts with metadata support.
    - Developed a tagging system for blog posts with dynamic routing.
    - Integrated RSS feed generation for blog posts.
    - Added sample blog posts to demonstrate functionality.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://joy40217.github.io',
-    base: 'astro-page'
+  site: 'https://joy40217.github.io',
+  base: 'astro-page',
+  integrations: [preact()]
 });
